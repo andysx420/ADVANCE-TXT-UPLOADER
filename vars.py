29e6c12121204b7917c5008a67ec539c8fa375c0
andysx420 <147619538+andysx420@users.noreply.github.com>
2025-07-11 19:41:16 +0530

    Update vars.py
    Removed credentials and error handling.

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24473318")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:AAEtg4obq7JHvsZ0TcA-PSBi0vcJABV54e4") #Replace with your bot token
+API_ID = int(environ.get("API_ID")) #Replace with your api id
+API_HASH = environ.get("API_HASH") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN") #Replace with your bot token
+
+if not all([API_ID, API_HASH, BOT_TOKEN]):
+    raise EnvironmentError("❌ Please set all required environment variables: API_ID, API_HASH, BOT_TOKEN")
